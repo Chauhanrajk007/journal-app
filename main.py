4f83168cb645bfe96bdbbb188a63996deb162caa
Chauhan Raj k <chauhanrajk007@gmail.com>
2025-06-04 12:57:26 +0530

    Update and rename app.py to main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render-template, request, jsonify
+from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 import firebase_admin
 from firebase_admin import credentials, auth, db
